d5310d71bb7a26702d4ac9955afb3ff15585e9ae
crismmgg <cristinammgg@gmail.com>
2020-02-13 13:29:12 +0100

    Update docs

---
+++ docs/precache-manifest.90a63de5bc047a80953b7e51b65ad2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2032584c39e47ba05538516c90e913",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb754caae2945029b876",
+    "url": "./static/css/main.e31b03bf.chunk.css"
+  },
+  {
+    "revision": "48f6681e3085f73ab0b5",
+    "url": "./static/js/2.83b1e622.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.83b1e622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb754caae2945029b876",
+    "url": "./static/js/main.4c235033.chunk.js"
+  },
+  {
+    "revision": "66e9c3bff573c32ca784",
+    "url": "./static/js/runtime-main.edafb34d.js"
+  }
+]);
